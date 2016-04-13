0f020c814604c0e5126b7e7b725095ba014484d4
Jay Byam <jrbyam@alaska.edu>
2016-04-13 15:34:15 -0800

    Changed draw in Circle to be correct PostScript

--- Circle.h
+++ Circle.h
@@ -8,7 +8,12 @@ class Circle : public Shape {
         this.radius = radius;
     }
     string draw() {
-        return ("0 360 " + radius + " arc");
+        return ("0 " +   // x position
+                "0 " +   // y postion
+                radius + // Radius length
+                " 0 " +  // Starting angle
+                "360 " + // Ending angle
+                " arc");
     }
 private:
     double radius = 0;
